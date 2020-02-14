5f76fd2bb88c2439eef575ff961997acc7cac716
Ercan Erdoğan <ercanerdogan@gmail.com>
2020-02-14 17:24:36 +0300

    todo app add new item

---
+++ script.js
@@ -0,0 +1,43 @@
+//UI vars
+
+const form = document.querySelector('form');
+const input = document.querySelector('#txtTaskName');
+const btnDeleteAll = document.querySelector('#btnDeleteAll');
+const taskList = document.querySelector("#task-list");
+
+eventListeners();
+
+function eventListeners(){
+    form.addEventListener('submit', addNewItem);
+}
+
+function addNewItem(e){
+    if (input.value==''){
+        alert('add new item');
+    }
+
+    //create li element
+    const li=document.createElement('li');
+    li.className='list-group-item list-group-item-secondary';
+    li.appendChild(document.createTextNode(input.value));
+
+    console.log(li);
+
+    //create a element
+    const a=document.createElement('a');
+    a.className='delete-item float-right';
+    a.setAttribute('href','#');
+    a.innerHTML = '<i class="fas fa-times"></i>';
+
+ 
+    //add a to li
+    li.appendChild(a);
+
+    taskList.appendChild(li);
+
+    input.value='';
+
+    e.preventDefault();
+
+    
+}
