547c5e2988923b683d067cc22bc4e927f3d7cd16
Ercan Erdoğan <ercanerdogan@gmail.com>
2020-02-28 18:04:35 +0300

    latest changes

--- script.js
+++ script.js
@@ -7,37 +7,66 @@ const taskList = document.querySelector("#task-list");
 
 eventListeners();
 
-function eventListeners(){
+function eventListeners() {
     form.addEventListener('submit', addNewItem);
+    taskList.addEventListener('click', deleteItem);
+    btnDeleteAll.addEventListener('click', deleteAllItems);
 }
 
-function addNewItem(e){
-    if (input.value==''){
+function addNewItem(e) {
+    if (input.value == '') {
         alert('add new item');
     }
 
     //create li element
-    const li=document.createElement('li');
-    li.className='list-group-item list-group-item-secondary';
+    const li = document.createElement('li');
+    li.className = 'list-group-item list-group-item-secondary';
     li.appendChild(document.createTextNode(input.value));
 
     console.log(li);
 
     //create a element
-    const a=document.createElement('a');
-    a.className='delete-item float-right';
-    a.setAttribute('href','#');
+    const a = document.createElement('a');
+    a.className = 'delete-item float-right';
+    a.setAttribute('href', '#');
     a.innerHTML = '<i class="fas fa-times"></i>';
 
- 
+
     //add a to li
     li.appendChild(a);
 
     taskList.appendChild(li);
 
-    input.value='';
+    input.value = '';
 
     e.preventDefault();
 
-    
+
+}
+
+function deleteItem(e) {
+    if (confirm('Are you sure want to delete item ?')) {
+        if (e.target.className === 'fas fa-times') {
+            e.target.parentElement.parentElement.remove();
+        }
+
+    }
+
+    e.preventDefault();
+}
+
+function deleteAllItems(e) {
+
+    if (confirm('Are you sure want to delete all items ?')) {
+        taskList.childNodes.forEach(function (item) {
+
+            if (item.nodeType === 1) {
+                 console.log(item);
+                item.remove();
+            }
+
+        });
+    }
+
+    e.preventDefault();
 }
